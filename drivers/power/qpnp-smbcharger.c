55251f8252d731f04a4faff59d2229dbf0d92543
richard0387 <richard-willess@hotmail.com>
2018-05-29 13:24:57 +0000

    Charge 2000Ma

--- drivers/power/qpnp-smbcharger.c
+++ drivers/power/qpnp-smbcharger.c
@@ -7797,7 +7797,7 @@ err:
 }
 
 #define DEFAULT_VLED_MAX_UV		3500000
-#define DEFAULT_FCC_MA			2500
+#define DEFAULT_FCC_MA			2000
 static int smb_parse_dt(struct smbchg_chip *chip)
 {
 	int rc = 0, ocp_thresh = -EINVAL;
